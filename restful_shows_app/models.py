dcf8575a165d4bfbb3532b6cbe135ea1984d128b
Chris <tobarchr@gmail.com>
2020-10-12 16:20:59 -0400

    update changes

--- restful_shows_app/models.py
+++ restful_shows_app/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from _datetime import date,datetime
+
+class showsManager(models.Manager):
+    def basic_validator(self, postData):
+        time_changed = datetime.strptime(postData['release_date'],"%Y-%m-%d")
+        now = datetime.now()
+        errors={}
+        if len(postData['title']) <2:
+            errors['title'] = "Title name should be at least 2 characters"
+
+        if len(postData['network']) <3:
+            errors['network'] = "Network should be at least 3 characters"
+
+        if len(postData ['desc'])<10 and len(postData ['desc'])>0:
+            errors['desc'] = "Description should be at least 10 characters"
+
+        if time_changed > now:
+            errors['release_date'] = "Release date cannot be future date"
+
+        return errors
 
 class shows(models.Model):
     title = models.CharField(max_length=255)
@@ -7,4 +27,5 @@ class shows(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = showsManager()
     
--- restful_shows_app/views.py
+++ restful_shows_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from restful_shows_app.models import *
 from datetime import datetime
+from django.contrib import messages
+
 
 def all_shows(request):
     show_list = shows.objects.all()
@@ -21,32 +23,40 @@ def new_show(request):
 
 def edit_page(request,edit_id):
     show_id = shows.objects.get(id=edit_id)
+    print(show_id.release_date)
     context = {
         "all_show_details" : show_id,
     }
     return render(request,'edit_show.html',context)
 
 def add_new(request):
-    title = request.POST['title']
-    network = request.POST['network']
-    release =request.POST['release_date']
-    print("*"*100)
-    print(release)
-    print("*"*100)
-    desc = request.POST['desc']
-    shows.objects.create(title=title,network=network,release_date=release,description = desc) 
-    return redirect('/shows/'+str(shows.objects.last().id))
+    errors = shows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        title = request.POST['title'] 
+        network = request.POST['network']
+        release =request.POST['release_date']
+        desc = request.POST['desc']
+        shows.objects.create(title=title,network=network,release_date=release,description = desc) 
+        return redirect('/shows/'+str(shows.objects.last().id))
 
 def update_show(request,show_id_to_update):
-    update_show_id = shows.objects.get(id=show_id_to_update)
-    print(update_show_id)
-    update_show_id.title = request.POST['title']
-    print(update_show_id.title)
-    update_show_id.network = request.POST['network']
-    update_show_id.release_date = request.POST['release_date']
-    update_show_id.description = request.POST['desc']
-    update_show_id.save()
-    return redirect('/shows/'+str(show_id_to_update))
+    errors = shows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/'+str(show_id_to_update)+'/edit')
+    else:
+        update_show_id = shows.objects.get(id=show_id_to_update)
+        update_show_id.title = request.POST['title']
+        update_show_id.network = request.POST['network']
+        update_show_id.release_date = request.POST['release_date']
+        update_show_id.description = request.POST['desc']
+        update_show_id.save()
+        return redirect('/shows/'+str(show_id_to_update))
 
 def destroy(request,delete_show_id):
     to_delete_id = shows.objects.get(id=delete_show_id)
